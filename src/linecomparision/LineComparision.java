da9ff9e4c8e10f9a254e2bfb2052f41a70a6beda
vinit <vinitsxn@gmail.com>
2021-12-23 21:09:31 +0530

    Added: Length Calculation Functionality

--- src/linecomparision/LineComparision.java
+++ src/linecomparision/LineComparision.java
@@ -1,7 +1,22 @@
 package linecomparision;
+import java.util.Scanner;
 
 public class LineComparision {
-    public static void main(String args[]){
+    public static double length(float x1,float y1,float x2,float y2){
+        return Math.sqrt(Math.pow((x1-x2),2)+(Math.pow((y2-y1),2)));
+    }
+    public static void main(String[] args){
         System.out.println("Welcome to Line Comparison Computation");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter coordinate X1: ");
+        float x1=sc.nextFloat();
+        System.out.println("Enter coordinate Y1: ");
+        float y1=sc.nextFloat();
+        System.out.println("Enter coordinate X2: ");
+        float x2=sc.nextFloat();
+        System.out.println("Enter coordinate Y2: ");
+        float y2=sc.nextFloat();
+        double length=length(x1,y1,x2,y2);
+        System.out.println("Length of the line ("+x1+","+y1+") and ("+x2+","+y2+") is: "+length);
     }
 }
