d5adf058a8846e50e067d7d078d9cb71d9db8427
vinit <vinitsxn@gmail.com>
2021-12-23 20:55:38 +0530

    Added: Welcome Message added to System

---
+++ src/linecomparision/LineComparision.java
@@ -0,0 +1,7 @@
+package linecomparision;
+
+public class LineComparision {
+    public static void main(String args[]){
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+}
